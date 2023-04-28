60a5bb0d48b84f3b549463b984d5fe7f227e1345
Ajay o s <ajayosakhub@gmail.com>
2023-04-28 08:48:07 +0530

    added commit

---
+++ Example/example.ts
@@ -0,0 +1,157 @@
+import { Boom } from '@hapi/boom'
+import NodeCache from 'node-cache'
+import makeWASocket, { AnyMessageContent, delay, DisconnectReason,  getAggregateVotesInPollMessage, makeCacheableSignalKeyStore, proto, useMultiFileAuthState, WAMessageContent, WAMessageKey } from './src'
+import { pino } from 'pino'
+const useStore = !process.argv.includes('--no-store')
+const doReplies = !process.argv.includes('--no-reply')
+
+
+const logger__ = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'hostname,pid,time', // Ignore these fields when pretty-printing
+            singleLine: false, // Do not pretty-print single-line logs
+            colorize: true, // Add color to the output
+            levelFirst: true, // Show the log level first in each log entry
+            append: true, // Append logs to the file instead of overwriting
+            mkdir: true, // Create the target directory if it doesn't exist
+        }
+    },
+    // level: process.env.NODE_ENV === 'production' ? 'info' : 'debug'
+    level: 'info' // Set the logging level to "info"
+});
+const logger = logger__.child({ level: 'silent' });
+const startSock = async() => {
+	const { state, saveCreds } = await useMultiFileAuthState('auth')
+
+	const sock = makeWASocket({
+		printQRInTerminal: true,
+		auth: {
+			creds: state.creds,
+			/** caching makes the store faster to send/recv messages */
+			keys: makeCacheableSignalKeyStore(state.keys, logger),
+		},
+		generateHighQualityLinkPreview: true,
+		// ignore all broadcast messages -- to receive the same
+		// comment the line below out
+		// shouldIgnoreJid: jid => isJidBroadcast(jid),
+		// implement to handle retries & poll updates
+		
+	})
+
+	const sendMessageWTyping = async(msg: AnyMessageContent, jid: string) => {
+		await sock.presenceSubscribe(jid)
+		await delay(500)
+
+		await sock.sendPresenceUpdate('composing', jid)
+		await delay(2000)
+
+		await sock.sendPresenceUpdate('paused', jid)
+
+		await sock.sendMessage(jid, msg)
+	}
+
+	// the process function lets you process all events that just occurred
+	// efficiently in a batch
+	sock.ev.process(
+		// events is a map for event name => event data
+		async(events) => {
+			// something about the connection changed
+			// maybe it closed, or we received all offline message or connection opened
+			if(events['connection.update']) {
+				const update = events['connection.update']
+				const { connection, lastDisconnect } = update
+				if(connection === 'close') {
+					// reconnect if not logged out
+					if((lastDisconnect?.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut) {
+						startSock()
+					} else {
+						console.log('Connection closed. You are logged out.')
+					}
+				}
+
+				console.log('connection update', update)
+			}
+
+			// credentials updated -- save them
+			if(events['creds.update']) {
+				await saveCreds()
+			}
+
+			if(events.call) {
+				console.log('recv call event', events.call)
+			}
+
+			// history received
+			if(events['messaging-history.set']) {
+				const { chats, contacts, messages, isLatest } = events['messaging-history.set']
+				console.log(`recv ${chats.length} chats, ${contacts.length} contacts, ${messages.length} msgs (is latest: ${isLatest})`)
+			}
+
+			// received a new message
+			if(events['messages.upsert']) {
+				const upsert = events['messages.upsert']
+				console.log('recv messages ', JSON.stringify(upsert, undefined, 2))
+
+				if(upsert.type === 'notify') {
+					for(const msg of upsert.messages) {
+						if(!msg.key.fromMe && doReplies) {
+							console.log('replying to', msg.key.remoteJid)
+							await sock!.readMessages([msg.key])
+							await sendMessageWTyping({ text: 'Hello there!' }, msg.key.remoteJid!)
+						}
+					}
+				}
+			}
+
+			// messages updated like status delivered, message deleted etc.
+			if(events['messages.update']) {
+				console.log(
+					JSON.stringify(events['messages.update'], undefined, 2)
+				)
+
+				
+				
+			}
+
+			if(events['message-receipt.update']) {
+				console.log(events['message-receipt.update'])
+			}
+
+			if(events['messages.reaction']) {
+				console.log(events['messages.reaction'])
+			}
+
+			if(events['presence.update']) {
+				console.log(events['presence.update'])
+			}
+
+			if(events['chats.update']) {
+				console.log(events['chats.update'])
+			}
+
+			if(events['contacts.update']) {
+				for(const contact of events['contacts.update']) {
+					if(typeof contact.imgUrl !== 'undefined') {
+						const newUrl = contact.imgUrl === null
+							? null
+							: await sock!.profilePictureUrl(contact.id!).catch(() => null)
+						console.log(
+							`contact ${contact.id} has a new profile pic: ${newUrl}`,
+						)
+					}
+				}
+			}
+
+			if(events['chats.delete']) {
+				console.log('chats deleted ', events['chats.delete'])
+			}
+		}
+	)
+
+	return sock
+
+}
+
+startSock()
